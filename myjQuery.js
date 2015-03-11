12dd120af2e0166355ac10d1acc906aaaa22a86a
Dan <dan@edgefall.com>
2015-03-11 17:47:46 -0400

    Add .load to properly get slide width

--- myjQuery.js
+++ myjQuery.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
 
+$(window).load()
+
 var currentSlideNumber = 0;
-var slideWidth = 900; 
+var slideWidth = 900;
+
+function calculateSlideWidth() {
+	slideWidth = $('#sliderBox .slide').first().outerWidth(true);
+}
 
 function moveSlides() {
 	$( "#sliderBox" ).stop(true).animate({right: slideWidth * currentSlideNumber });
 }
 
+$(window).load(function() {
+	calculateSlideWidth();
+});
+
 $(window).resize(function() {
-	slideWidth = $('#sliderBox .slide').first().outerWidth(true);
+	calculateSlideWidth();
 	moveSlides();
 });
 
